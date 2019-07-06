64aa291bd4e52979433968dd4e02d16fa43d1a99
ksmorano <ksmorano@gmail.com>
2019-07-06 22:44:15 +0800

    encourage me(feat): integrate voice and video

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,9 @@
 import React from "react";
+import classNames from "classnames";
+// import styles from "./layout.module.scss";
 
 const Layout = ({ children }) => (
-  <section className="app p-5">
+  <section className={classNames("app p-5")}>
     <div className="columns is-centered">
       <div className="column is-half">{children}</div>
     </div>
