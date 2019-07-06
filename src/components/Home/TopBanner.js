52b1af47d173c2adc74952711ba18892f8a9bd5c
cayeborreo <cborreo@medgrocer.com>
2019-07-07 02:00:29 +0800

    login(fix): fix post

--- src/components/Home/TopBanner.js
+++ src/components/Home/TopBanner.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const TopBanner = () => {
-  return "This is the TopBanner";
+  return (
+    <div className="field is-grouped mb-4">
+      <p className="control is-expanded">
+        <h2 className="is-size-3">My Past Rawrs</h2>
+      </p>
+      {/* <button className="button is-info mt-1">Sign out</button> */}
+    </div>
+  );
 };
 
 export default TopBanner;
