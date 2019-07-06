62f80d4564ebcdc11db3885b94e7b247ec403fea
cayeborreo <cborreo@medgrocer.com>
2019-07-06 22:43:43 +0800

    how-are-you(feat): input forms

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import addNoteReducer from "./../components/HowAreYou/reducers";
 
 const rootReducer = combineReducers({
   // add reducers here
   // todo: todoReducer
+  addNoteReducer
 });
 
 export default rootReducer;
