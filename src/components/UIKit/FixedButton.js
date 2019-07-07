45da7b9e32f6cbb6a6ec61859218cabc0addd4fe
ksmorano <ksmorano@gmail.com>
2019-07-07 09:49:20 +0800

    hide fixed button on keyboard open

--- src/components/UIKit/FixedButton.js
+++ src/components/UIKit/FixedButton.js
@@ -1,8 +1,13 @@
 import React from "react";
+import classNames from "classnames";
 import styles from "./uikit.module.scss";
 
 const FixedButton = ({ children }) => {
-  return <div className={styles.fixedButton}>{children}</div>;
+  return (
+    <div className={classNames("fixedButton", styles.fixedButton)}>
+      {children}
+    </div>
+  );
 };
 
 export default FixedButton;
